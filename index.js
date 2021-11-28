8e1326bae272e18d14f3269e35885ce7de6591a4
YuLim2 <jyulim0120@gmail.com>
2021-11-28 23:46:53 +0900

    MongoDB 연결

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://YuLim2:<password>@firstcluster.qtt7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-})
+mongoose.connect(`mongodb+srv://YuLim2:sun138814!@firstcluster.qtt7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')

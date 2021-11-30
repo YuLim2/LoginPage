bb233eb4e692010a21a3fd1562071f2050c726b6
YuLim2 <jyulim0120@gmail.com>
2021-11-30 23:06:36 +0900

    한국어 출력으로 변경

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://YuLim2:sun138814!@firstcluster.qtt7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(`mongodb+srv://YuLim2:sun138814%21@firstcluster.qtt7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, ).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('하이!')
 })
 
 app.listen(port, () => {

469ce5da9b0621575ff85f07135655fa0d0020b7
YuLim2 <jyulim0120@gmail.com>
2021-11-30 22:58:01 +0900

    모듈 추가

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const monggose = require('mongoose');
+
+const userSchema = monggose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    Image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = monggose.model('User', userSchema) //스키마를 모델로 감싸기
+
+module.exports = { User } //다른 파일에서도 쓸 수 있도록 export

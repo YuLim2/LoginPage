6e6d54c05daa6d77b7c59220e9fdc3a15bcefa7c
YuLim2 <jyulim0120@gmail.com>
2021-12-01 00:08:13 +0900

    client에서 user 정보 가져오고 post 방식으로 db에 저장하고 성공, 실패 결과 도출

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser'); //body-parser 불러오기
+const { User } = require("./models/User"); //user 모델 불러오기
+
+//application/x-ww-form-urlencoded 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 분석해서 가져옴
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect(`mongodb+srv://YuLim2:sun138814%21@firstcluster.qtt7c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, ).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => {
     res.send('하이!')
+})
+
+app.post('/register', (req, res) => { //client에서 user 정보 가져오고 db에 저장
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true }) //200메소드 응답
+    })
 })
 
 app.listen(port, () => {

cfaa9255a1ef4a2ec0387b00fa8868555d560ee5
YuLim2 <jyulim0120@gmail.com>
2021-12-04 21:23:04 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser'); // body-parser 불러오기
 
 const config = require("./config/key");
 
-const { User } = require("./models/User"); // user 모델 불러오기
 
 // application/x-ww-form-urlencoded 분석해서 가져옴
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,14 +14,17 @@ app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
-mongoose.connect(config.mongoURL) // db 연결 
+mongoose.connect(config.mongoURL) // db 연결
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => { // root일 때
-    res.send('하이!_수정사항')
+app.get('/', (req, res) => { // root일 때   
+    res.send('하이!_수정사항 ')
 })
+
+
+const { User } = require("./models/User"); // user 모델 불러오기
 
 app.post('/register', (req, res) => { // client에서 user 정보 가져오고 db에 저장
     const user = new User(req.body)
